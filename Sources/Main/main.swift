8fe19eabdfa2865bcc585d3f8dd7a1b71418ef3d
Toni Suter <tonisuter@me.com>
2020-01-19 14:46:38 +0100

    Run swiftformat

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -4,13 +4,12 @@ var stack1 = Stack<Int>(capacity: 5)
 stack1.push(10)
 stack1.push(20)
 stack1.push(30)
-print("stack1: ", stack1)       // stack1:  [10, 20, 30]
+print("stack1: ", stack1)
 
-var stack2 = stack1             // At this point, both stacks share the same buffer.
-print("stack1: ", stack1)       // stack1:  [10, 20, 30]
-print("stack2: ", stack2)       // stack2:  [10, 20, 30]
+var stack2 = stack1 // At this point, both stacks share the same buffer.
+print("stack1: ", stack1)
+print("stack2: ", stack2)
 
-stack2.pop()                    // To maintain value semantics, stack2 clones the shared buffer
-                                // before performing the pop() operation.
-print("stack1: ", stack1)       // stack1:  [10, 20, 30]
-print("stack2: ", stack2)       // stack2:  [10, 20]
+stack2.pop() // To maintain value semantics, stack2 clones the shared buffer before performing the pop() operation.
+print("stack1: ", stack1)
+print("stack2: ", stack2)
